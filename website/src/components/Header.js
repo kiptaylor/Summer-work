5db5313301ae02a0b1c7d21bedbe60e1fdda5496
kip taylor <kip.taylor123@gmail.com>
2021-06-18 10:48:03 -0400

    fixed
    added alot and took out a lot

---
+++ website/src/components/Header.js
@@ -0,0 +1,15 @@
+import Button from './Button' 
+
+const Header = ({ title, onAdd, showAdd }) => {
+    return (
+        <header className= 'header'>
+           <h1 style={{ color:'black'}}>{title}</h1> 
+           <Button
+                color={showAdd ? 'black' : 'green'}
+                text={showAdd ? 'Close' : 'Add'} 
+                onClick= {onAdd}/>
+        </header>
+    )
+}
+// add static data for button 
+export default Header
