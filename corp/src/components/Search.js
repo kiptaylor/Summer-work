a4457ff61c5f62ffa57c990660a29f95d8d0fde7
kiptaylor <kip.taylor123@gmail.com>
2021-07-22 10:45:11 -0400

    testing

--- corp/src/components/Search.js
+++ corp/src/components/Search.js
@@ -8,24 +8,28 @@ import { render } from '@testing-library/react';
 <img src={logo} height={100} width={100} />
 
 const Search = () => {
+    state ={
+        errorMessage = ''
+    }
     const [data, setData] = React.useState(null)
     const [value, setValue] = useState(0);
+    const [err, setError] = useState(false);
     const buttonOnClick = (event) => {
         event.preventDefault();
         fetch(`http://localhost:8080/api/coupa_reinjection/po?po-number=${value}`)
             .then(response => response.json())
             .then((result) => {
                 setData(result)
-            }).catch((error) => {
-                console.log('error occurred: ', error)
-            })
-    }
+            }) .catch(err => 
+                useState({errorMessage: err.messege}))
     return (
         <form action="/" method="get">
             <label htmlFor="header-search">
                 <header>
                 <img className='logo' src={logo} alt=""/>
                 </header>
+                {setError(errorMessage &&
+                <h3 className="error"> { useState(err) } </h3>) }
                 <span className="visually-hidden">Input PO Number: </span>
             </label>
             < IntegerInput value={ value } min={-1} max={999999999} onChange={ (value) => setValue(value) }
